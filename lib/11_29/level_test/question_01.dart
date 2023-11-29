2e00cd0b30fddce3a3faf5ac1d1045bb5b1ea6f5
YongYeon Kim <assavictory@gmail.com>
2023-11-29 12:12:07 +0900

    11.29 레벨테스트

---
+++ lib/11_29/level_test/question_01.dart
@@ -0,0 +1,22 @@
+/*
+주어진 문자열이 회문(palindrome)인지 확인하는 함수를 작성하세요. 회문이란, 거꾸로 읽어도 원래와 같은 단어나 문장을 말합니다.
+예시: "racecar"는 회문입니다. "hello"는 회문이 아닙니다.
+ */
+
+void main() {
+  String checkPalindrome = "racecare";
+
+  List<String> target = checkPalindrome.split("");
+
+  num test = target.length / 2;
+  bool result = false;
+
+  for(int i = 0;i < test.floor();i++) {
+    var order = target[i];
+    var reverseOrder = target[target.length - 1 - i];
+
+    result = order == reverseOrder;
+  }
+
+  print("'$checkPalindrome'는 ${result ? "회문 입니다." : "회문이 아닙니다."}");
+}
---
+++ lib/11_29/level_test/question_02.dart
@@ -0,0 +1,28 @@
+/*
+주어진 정수 배열 또는 리스트에서 가장 큰 두 수를 찾아 더한 값을 반환하는 함수를 작성하세요.
+예시: [1, 2, 5, 3, 9, 8] 이 주어졌을 때, 가장 큰 두 수는 9와 8이므로 9 + 8 = 17이 반환되어야 합니다.
+ */
+
+void main() {
+  List<int> target = [1, 2, 5, 3, 9, 8];
+
+  int result = sum(target);
+
+  print(result);
+}
+
+int sum(List<int> target){
+  int first = 0;
+  int second = 0;
+
+  for(int i = 0;i < target.length;i++) {
+    int current = target[i];
+    if ( current > first ) {
+      first = current;
+    } else if ( current > second ) {
+      second = current;
+    }
+  }
+
+  return first + second;
+}
