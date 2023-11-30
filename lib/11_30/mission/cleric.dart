51a420a764aec8a6dcc2a852444af30409bf4e6a
YongYeon Kim <assavictory@gmail.com>
2023-11-30 16:58:53 +0900

    연습문제 1-3 : 셀프에이드 스킬 셋팅

--- lib/11_30/mission/cleric.dart
+++ lib/11_30/mission/cleric.dart
@@ -7,4 +7,14 @@ class Cleric {
   int mp = 10;
 
   Cleric(this.name);
+
+  void selfAid() {
+    if (mp < 5) {
+      print("mp가 부족합니다.");
+    } else {
+      print("hp가 ${maxHp - hp} 회복 되었습니다.");
+      mp -= 5;
+      hp = maxHp;
+    }
+  }
 }
--- lib/11_30/mission/main.dart
+++ lib/11_30/mission/main.dart
@@ -1,5 +1,9 @@
 import 'cleric.dart';
 
 main() {
-  Cleric cleric = Cleric("이름");
+  Cleric cleric = Cleric("11_김용연");
+
+  cleric.selfAid();
+  cleric.selfAid();
+  cleric.selfAid();
 }
