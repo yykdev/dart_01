95a560885fa9f727cfae7db28f6754e3c1285ce3
YongYeon Kim <assavictory@gmail.com>
2023-12-01 12:27:54 +0900

    프로젝트 구조 변경

--- lib/game/cleric.dart
+++ lib/game/cleric.dart
@@ -39,3 +39,16 @@ class Cleric {
     return recovery;
   }
 }
+
+main() {
+  Cleric cleric = Cleric("11_김용연");
+
+  cleric.selfAid();
+  cleric.selfAid();
+  cleric.selfAid();
+  cleric.pray(5);
+  cleric.pray(5);
+
+  print("-------------");
+  print("hp : ${cleric.hp} , mp : ${cleric.mp}");
+}
