5644df7eeb7f5b5905215678c7197215aa85fec1
YongYeon Kim <assavictory@gmail.com>
2023-12-04 15:41:34 +0900

    연습문제 10-1

---
+++ lib/23_12_04/00_collection.dart
@@ -0,0 +1,16 @@
+void main() {
+  // List : 리스트
+  // List<자료형> 리스트명 = [값, ... ];
+  // final 리스트명 = <자료형>[값, ... ];
+
+  // 일관 된 자료형으로 생성
+  final List<int> numbers1 = [];
+  numbers1.add(1);
+  numbers1.add(2);
+
+  final List<String> text = [];
+  text.add("1");
+  text.add("2");
+
+
+}
--- lib/game/wizard.dart
+++ lib/game/wizard.dart
@@ -1,6 +1,18 @@
 class Wizard {
   String name;
   int hp;
+  Wand? wand;
 
-  Wizard(this.name, this.hp);
+  Wizard(this.name, this.hp, this.wand);
+}
+
+class Wand {
+  String name; // 이름
+  double power; // 마력
+
+  Wand(this.name, this.power);
+}
+
+void main() {
+
 }
